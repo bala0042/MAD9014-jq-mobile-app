7a14e567aace55bbc46971c611bf351d4f4d04c8
Noyan Balay <bala0042@algonquinlive.com>
2015-11-25 18:00:13 -0500

    commented out console logs

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ var page = localStorage.getItem('page');
 if(!page){
 	page = "win1";
 }
-console.log(page);
+//console.log(page);
 	
 $(document).on( "pageshow", function() {
 	$("#startTut").off("click").click(function(e){
@@ -15,6 +15,6 @@ $(document).on("pagechange", function() {
 	var pageid = $.mobile.activePage.attr('id');
 	if(typeof(Storage) !== "undefined" && pageid != "home" && pageid != "info"){
 		localStorage.setItem('page', pageid);
-		console.log(pageid);
+		//console.log(pageid);
 	}
 });
