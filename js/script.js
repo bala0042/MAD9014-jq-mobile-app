f386ed31d6974c2fc900bfe17b2a5753d3d6ce19
Noyan Balay <bala0042@algonquinlive.com>
2015-11-25 17:59:34 -0500

    fixes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-$(document).on( "pageshow", function() {
-	var page = localStorage.getItem('page');
+var page = localStorage.getItem('page');
+if(!page){
+	page = "win1";
+}
+console.log(page);
 	
+$(document).on( "pageshow", function() {
 	$("#startTut").off("click").click(function(e){
 		e.preventDefault();
-		
-		if(!page){
-			page = "win1";
-		}
-		
 		$( ":mobile-pagecontainer" ).pagecontainer( "change", "#"+page );
 	});
 });
 
 $(document).on("pagechange", function() {
 	var pageid = $.mobile.activePage.attr('id');
-	
 	if(typeof(Storage) !== "undefined" && pageid != "home" && pageid != "info"){
 		localStorage.setItem('page', pageid);
-		//console.log(pageid);
+		console.log(pageid);
 	}
 });
